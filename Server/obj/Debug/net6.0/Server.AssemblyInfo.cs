eac0300fb33fdde1e5851d2f483512b4a2bbd7f7
Masha Dubrovsky <mashadubrovsky@gmail.com>
2025-08-06 19:55:59 +0300

    Masha 6/8
    bdika

--- Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f3eccfc7cf728d871da6a82e90bf26b51228e78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31c778d0a62153b8f101bad0e0580167f6ba6a3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
