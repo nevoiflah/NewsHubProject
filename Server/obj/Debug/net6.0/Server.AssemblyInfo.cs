7247db3cde3739a82a1bac67504daadbafe8920e
Masha Dubrovsky <mashadubrovsky@gmail.com>
2025-08-07 17:41:11 +0300

    features changes

--- Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e2e0ba865055c5dd47dbfd55bf52b9ec911e900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+306ac9b63999d2ab84e4ad48b4f81f39319a1d0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
